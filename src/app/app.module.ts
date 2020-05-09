4316b69377a7c0c4b27c2cce9f790c68e7b36998
test <test@test.be>
2020-05-09 15:32:09 +0200

    noice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardViewComponent } from './components/card-view/card-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardViewComponent
   ],
   imports: [
     BrowserModule,
