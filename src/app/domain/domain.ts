d2a1f71e667819b8a9ea98a6bcca9d7eb2d7382f
test <test@test.be>
2020-05-10 14:44:50 +0200

    no

--- src/app/domain/domain.ts
+++ src/app/domain/domain.ts
@@ -1,14 +1,20 @@
 import { Dealer as DealerAggregate, Deck as DeckAggregate, PlayerAggregate, TurnAggregate } from '@domain/aggregates';
 import { Injectable } from '@angular/core';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class Domain {
-    public constructor(public dealer: DealerAggregate, public deck: DeckAggregate, public player: PlayerAggregate, public turn: TurnAggregate) {}
+  
+  public constructor(
+    public dealer: DealerAggregate,
+    public deck: DeckAggregate,
+    public player: PlayerAggregate,
+    public turn: TurnAggregate
+  ) { }
 
-    public init() {
-        this.dealer.init();
-        this.deck.init();
-        this.player.init();
-        this.turn.init();
-    }
+  public init() {
+    this.dealer.init();
+    this.deck.init();
+    this.player.init();
+    this.turn.init();
+  }
 }
