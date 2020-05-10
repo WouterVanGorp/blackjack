ec659af9a7b6df8addaf5930135744dd9b0e703b
test <test@test.be>
2020-05-10 16:02:56 +0200

    noice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 import { map, filter, tap, startWith } from 'rxjs/operators';
 import { Observable } from 'rxjs';
@@ -15,18 +15,20 @@ import { Publisher, RequestCardEvent, PassEvent } from '@domain/events';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+
   dealer$: Observable<{ hand: Hand, role: PlayerType }>;
   player$: Observable<{ hand: Hand, role: PlayerType }>;
+
+  gameOngoing: boolean = false;
 
   constructor(
     private publisher: Publisher,
     private domain: Domain,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.domain.init();
     this.setupListeners();
-    this.publisher.publish(StartEvent);
   }
 
   private setupListeners() {
@@ -35,7 +37,6 @@ export class AppComponent implements OnInit {
         tap(x => console.log('test')),
         filter(p => p.for === PlayerType.Dealer),
         map(p => ({ hand: p.newHand, role: p.for })),
-        startWith({ hand: new Hand() , role: PlayerType.Dealer }),
       );
 
     this.publisher.listen(HandUpdatedEvent).pipe(filter(p => p.for === PlayerType.Dealer)).subscribe(x => console.log(x));
@@ -45,15 +46,18 @@ export class AppComponent implements OnInit {
         tap(x => console.log('test')),
         filter(p => p.for === PlayerType.Player),
         map(p => ({ hand: p.newHand, role: p.for })),
-        startWith({ hand: new Hand() , role: PlayerType.Player }),
       );
   }
 
-  userAction(action : 'HIT' | 'PASS') {
-    if(action === 'HIT') this.onHit();
-    else if(action === 'PASS') this.onPass();
+  userAction(action: 'HIT' | 'PASS') {
+    if (action === 'HIT') this.onHit();
+    else if (action === 'PASS') this.onPass();
   }
 
   onHit = () => this.publisher.publish(RequestCardEvent, new RequestCardEvent({ who: PlayerType.Player }));
   onPass = () => this.publisher.publish(PassEvent, new PassEvent({ who: PlayerType.Player }));
+  start = () => {
+    this.publisher.publish(StartEvent);
+    this.gameOngoing = true;
+  }
 }
