253e65e055c46ab72c176d52eefaa9e346d238ea
test <test@test.be>
2020-05-11 15:56:02 +0200

    value

--- src/app/domain/entities/hand.entity.ts
+++ src/app/domain/entities/hand.entity.ts
@@ -3,7 +3,7 @@ import { Entity } from './entity';
 
 export class Hand extends Entity {
     public cards: Card[] = [];
-    public value: number[] = [0];
+    public value: number = 0;
 
     public add(...cards: Card[]) {
         this.cards.push(...cards);
